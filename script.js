dde6c30916fea17a8abe7d3898a5c401baf455d2
maitbor <matiborrahaman@gmail.com>
2021-10-16 10:44:41 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// ***Mathmatical Oparation in Javascript***
+
+// Adding
+
+var price1 = 12;
+var price2 = 8;
+var total = price1 + price2;
+console.log(total);
+
+
+// Min
+
+var price1 = 20;
+var price2 = 5;
+var total = price1 - price2;
+console.log(total);
+
+// Multi
+
+var price1 = 20;
+var price2 = 5;
+var total = price1 * price2;
+console.log(total);
+
+
+// Div
+var price1 = 1000;
+var price2 = 50;
+var total = price1 / price2;
+console.log(total);
